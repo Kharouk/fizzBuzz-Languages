a93b7b49ff0fb6e2c1f3c5c5ad67a37a4131deed
Alex Kharouk <akharoukpp@gmail.com>
2018-11-02 10:07:25 +0000

    write first c sharp test

---
+++ c-sharp/unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int digit)
+        {
+            throw new NotImplementedException("Please Create a Test First!");
+        }
+    }
+}
