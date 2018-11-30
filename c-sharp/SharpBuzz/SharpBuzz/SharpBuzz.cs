6de422193412f4864fb082e1629f857e3a21b09d
Alex Kharouk <akharoukpp@gmail.com>
2018-11-30 09:56:33 +0000

    Begin TDDing with DivisibleBy3

---
+++ c-sharp/SharpBuzz/SharpBuzz/SharpBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace SharpBuzz
+{
+    public class SharpBuzz
+    {
+        public void Main()
+        {
+        }
+
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+        public bool DivisibleBy3(int num)
+        {
+            if (num % 3 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ c-sharp/SharpBuzz/SharpBuzz/SharpBuzzTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+namespace SharpBuzz
+{
+    [TestFixture()]
+    public class SharpBuzzTest
+    {
+        private SharpBuzz Sharp = new SharpBuzz();
+
+        [Test()]
+        public void SharpBuzz_Works()
+        {
+            Assert.AreEqual(Sharp.Hello(), "Hello World!");
+        }
+
+        [Test()]
+        public void SharpBuzz_divisibleBy3()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy3(3), true);
+        }
+
+        [Test()]
+        public void SharpBuzz_notDivisibleBy3()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy3(5), false);
+        }
+    }
+}
