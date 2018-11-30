d85f8bec4b5c6c6a89ef13f9a0d3fbea26d1d6c7
Alex Kharouk <akharoukpp@gmail.com>
2018-11-30 10:04:44 +0000

    Divisible up to 15

--- c-sharp/SharpBuzz/SharpBuzz/SharpBuzz.cs
+++ c-sharp/SharpBuzz/SharpBuzz/SharpBuzz.cs
@@ -3,10 +3,6 @@ namespace SharpBuzz
 {
     public class SharpBuzz
     {
-        public void Main()
-        {
-        }
-
         public string Hello()
         {
             return "Hello World!";
@@ -22,6 +18,18 @@ namespace SharpBuzz
             {
                 return false;
             }
+        }
+
+        public bool DivisibleBy5(int num)
+        {
+            // Can be refactored to this:
+            return num % 5 == 0;
+        }
+
+
+        public bool DivisibleBy15(int num)
+        {
+            return num % 5 == 0 && num % 3 == 0;
         }
     }
 }
--- c-sharp/SharpBuzz/SharpBuzz/SharpBuzzTest.cs
+++ c-sharp/SharpBuzz/SharpBuzz/SharpBuzzTest.cs
@@ -2,27 +2,51 @@
 using System;
 namespace SharpBuzz
 {
-    [TestFixture()]
+    [TestFixture]
     public class SharpBuzzTest
     {
         private SharpBuzz Sharp = new SharpBuzz();
 
-        [Test()]
+        [Test]
         public void SharpBuzz_Works()
         {
             Assert.AreEqual(Sharp.Hello(), "Hello World!");
         }
 
-        [Test()]
+        [Test]
         public void SharpBuzz_divisibleBy3()
         {
             Assert.AreEqual(Sharp.DivisibleBy3(3), true);
         }
 
-        [Test()]
+        [Test]
         public void SharpBuzz_notDivisibleBy3()
         {
             Assert.AreEqual(Sharp.DivisibleBy3(5), false);
+        }
+
+        [Test]
+        public void SharpBuzz_divisibleBy5()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy5(10), true);
+        }
+
+        [Test]
+        public void SharpBuzz_notDivisibleBy5()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy5(11), false);
+        }
+
+        [Test]
+        public void SharpBuzz_DivisibleBy15()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy15(30), true);
+        }
+
+        [Test]
+        public void SharpBuzz_NotDivisibleBy15()
+        {
+            Assert.AreEqual(Sharp.DivisibleBy15(31), false);
         }
     }
 }
